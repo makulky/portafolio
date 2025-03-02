db6f789fe02bfe6bd60d0e019e8890b286cd4989
Miguel <miguelfiguerolaalonso@gmail.com>
2025-03-02 18:59:53 +0100

    añadido contactos y pie de pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { MainComponent } from "./main/main.component";
 import { ContactComponent } from "./contact/contact.component";
 import { SkillsComponent } from "./skills/skills.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, MainComponent, ContactComponent, SkillsComponent],
+  imports: [NavbarComponent, MainComponent, ContactComponent, SkillsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
