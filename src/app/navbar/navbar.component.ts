f65a8e6c98984a639468232d98722716ee30d3fa
Miguel <miguelfiguerolaalonso@gmail.com>
2025-03-03 16:14:54 +0100

    solucionado responsive menu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,29 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { SkillsComponent } from '../skills/skills.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  menuOpen: boolean = false;
-  activeSection: string = ''; // Almacena la sección activa
+  menuOpen: boolean = false; // Controla el estado del menú colapsable
+  activeMenu: string = ''; // Almacena el enlace activo
 
+  // Alterna la visibilidad del menú
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
-  activeMenu: string = '';
-  
- // Almacena el enlace activo
 
-  setActive(menuItem: string, section:string) {
-    this.activeSection = section; // Cambia el estado de la sección activa
-    document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
-    this.activeMenu = menuItem; // Cambia el estado del menú activo
+  // Establece el enlace activo y realiza el desplazamiento
+  setActive(menuItem: string) {
+    this.activeMenu = menuItem; // Actualiza el menú activo
+    document.getElementById(menuItem)?.scrollIntoView({ behavior: 'smooth' });
+    this.menuOpen = false; // Cierra el menú en dispositivos móviles al seleccionar un enlace
   }
 }
